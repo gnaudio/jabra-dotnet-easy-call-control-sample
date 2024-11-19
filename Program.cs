b8a365728d7a7be4b907d84d5f46821f7212e7a9
Peter Høy Andersen <pehandersen@jabra.com>
2024-11-19 11:21:01 +0100

    Attempts at connection management. Needs further work.

--- Program.cs
+++ Program.cs
@@ -6,16 +6,34 @@ using Jabra.NET.Sdk.Modules.EasyCallControl.Types;
 
 internal class Program
 {
+    class EccState
+    {
+        public MuteState IsMuted { get; set; }
+        public HoldState IsOnHold { get; set; }
+        public uint OngoingCalls { get; set; }
+        public bool IsRinging { get; set; }
+    }
+
+    // Keeping track of the current state of Easy Call Control.
+    private static EccState currentEccState = new EccState();
+
+    // Keeping track of the current device that Easy Call Control is connected to.
+    private static IDevice currentEccDevice;
+
     private static IManualApi jabraSdk;
     private static EasyCallControlFactory easyCallControlFactory;
     // Using MultiCallControl to handle multiple calls. Even if you only need to handle single call use cases, you should use MultiCallControl.
-    private static IMultiCallControl multiCallControl;
+    private static IMultiCallControl easyCallControl;
 
     public static async Task Main()
     {
-        Console.WriteLine("Jabra .NET SDK Easy Call Control Sample app starting. Press ctrl+c or close the window to end.\n");
+        // Writing available commands to console 
+        PrintMenu();
 
-        //Initialize the core SDK. Recommended to use Init.InitManualSdk(...) (not Init.Init(...)) to allow setup of listeners before the SDK starts discovering devices.
+        // Start the key press listener in a separate task
+        var keyPressTask = Task.Run(() => ListenForKeyPress(), CancellationToken.None);
+
+        // Initialize the core SDK. Recommended to use Init.InitManualSdk(...) (not Init.Init(...)) to allow setup of listeners before the SDK starts discovering devices.
         var config = new Config(
             partnerKey: "get-partner-key-at-developer.jabra.com",
             appId: "JabraEasyCallControlSample",
@@ -24,63 +42,258 @@ internal class Program
         jabraSdk = Init.InitManualSdk(config);
         easyCallControlFactory = new EasyCallControlFactory(jabraSdk);
 
-        //Subscribe to SDK log events.
+        // Subscribe to SDK log events.
         jabraSdk.LogEvents.Subscribe((log) =>
         {
-            if (log.Level == LogLevel.Error) Console.WriteLine(log.ToString());
-            //Ignore info, warning, and debug log messages.
+            // Ignore info, warning, and debug log messages.
+            if (log.Level == LogLevel.Error) OutPutToConsole(log.ToString());
         });
 
-        //Setup listeners for Jabra devices being attached/detected.
+        // Setup listeners for Jabra devices being attached/detected.
         SetupDeviceListeners();
 
         // Enable the SDK's device discovery AFTER listeners and other necessary infrastructure is setup.
         await jabraSdk.Start();
-        Console.WriteLine("Now listening for Jabra devices...\n");
+        OutPutToConsole("Now listening for Jabra devices...\n");
 
-        //Keep the sample app running until actively closed.
+        // Keep the sample app running until actively closed.
         Task.Delay(-1).Wait();
     }
 
     static void SetupDeviceListeners()
     {
-        //Subscribe to Jabra devices being attached/detected by the SDK
-        jabraSdk.DeviceAdded.Subscribe(async (IDevice device) =>
+        // Subscribe to Jabra devices being attached/detected by the SDK
+        jabraSdk.DeviceAdded.Subscribe(static async (IDevice device) =>
         {
-            Console.WriteLine($"> Device attached/detected: {device.Name} (Product ID: {device.ProductId}, Serial #: {device.SerialNumber})");
+            OutPutToConsole($"> Device attached/detected: {device.Name} (Product ID: {device.ProductId}, Serial #: {device.SerialNumber})");
 
             // If the device supports Easy Call Control, enable it.
-            if (easyCallControlFactory.SupportsEasyCallControl(device)) 
+            if (easyCallControlFactory.SupportsEasyCallControl(device))
             {
-                Console.WriteLine("Setting up Easy Call Control for device: " + device.Name);
-                multiCallControl = await easyCallControlFactory.CreateMultiCallControl(device);
-                CommandLineInterface();
+                OutPutToConsole("Setting up Easy Call Control for device: " + device.Name);
+                // Setting values of currentEccState to default values
+                currentEccState = new EccState()
+                {
+                    IsMuted = MuteState.NoOngoingCalls,
+                    IsOnHold = HoldState.NoOngoingCalls,
+                    OngoingCalls = 0,
+                    IsRinging = false
+                };
+                currentEccDevice = device;
+                SetupEcc(device, null);
+
+                // Listen for disconnect event for headset. Disconnect can happen when the device has multiple connections
+                // such as a BT headset connected via the dongle and a USB cable at the same time. In these cases we need to handle the 
+                // switching between the connections in case for example the USB connection is disconnected.
+                device.ConnectionRemoved.Subscribe(async (connection) => 
+                {
+                    if (!easyCallControl.Connection.IsConnected)
+                    {
+                        // Active connection for Easy Call Control was removed. Teardown Easy Call Control and set up again with initial state.
+                        easyCallControl.Teardown();
+                        if (easyCallControlFactory.SupportsEasyCallControl(device))
+                        {
+                            OutPutToConsole($"One of the ways the headset was connected was removed. \n" +
+                                $"Other connection still active - setting up ECC again with previous state for: {device.Name}");
+                            bool initialMuteState = (currentEccState.IsMuted == MuteState.Muted);
+                            bool initialHoldState = (currentEccState.IsOnHold == HoldState.OnHold);
+                            MultiInitialState initialState = new MultiInitialState
+                            (
+                                ongoingCalls: currentEccState.OngoingCalls,
+                                isMuted: initialMuteState,
+                                isOnHold: initialHoldState
+                            );
+                            SetupEcc(device, initialState);
+                        }
+                    }
+                    else
+                    {
+                        OutPutToConsole($"One of the ways the headset was connected was removed. \n" +
+                            $"ECC was set up for another connection so is still active for: {device.Name}");
+                    }
+                });
             }
             else
             {
-                Console.WriteLine("Easy Call Control is not supported for device: " + device.Name);
+                OutPutToConsole("Easy Call Control is not supported for device: " + device.Name);
             }
         });
 
         //Subscribe to Jabra devices being detached/rebooted
         jabraSdk.DeviceRemoved.Subscribe((IDevice device) =>
         {
-            Console.WriteLine($"< Device detached: {device.Name} (Product ID: {device.ProductId}, Serial #: {device.SerialNumber})");
+            OutPutToConsole($"< Device detached: {device.Name} (Product ID: {device.ProductId}, Serial #: {device.SerialNumber})");
+            if (currentEccDevice == device)
+            {
+                OutPutToConsole("Removed current ECC headset: " + device.Name);
+                // Here we should look through available devices and see if we can find another device to connect to.
+                // TO DO - Implement logic to connect to another device if available.
+
+            }
         });
     }
-    static void CommandLineInterface()
+
+        async static void SetupEcc(IDevice device, MultiInitialState? initialState)
     {
-        Console.WriteLine("Enter a command:");
-        var userSelection = Console.ReadKey(intercept: true);
-        switch (userSelection.KeyChar)
+        // Create Easy Call Control instance for the device
+        if (initialState == null)
         {
-            case '1':
-                Console.WriteLine("keypressed 1");
-                break;
-            default:
-                Console.WriteLine("Invalid command.");
-                break;
+            easyCallControl = await easyCallControlFactory.CreateMultiCallControl(device);
+        }
+        else
+        {
+            easyCallControl = await easyCallControlFactory.CreateMultiCallControl(device, initialState);
         }
 
+        // Subscribe to Easy Call Control events. By listening to these events your application
+        // can keep track of the call state of the device and update its UI accordingly.
+
+        // OngoingCalls is the number of ongoing calls on the device. This includes active and held calls. 
+        easyCallControl.OngoingCalls.Subscribe((ongoingCalls) =>
+        {
+            OutPutToConsole($"Ongoing calls: {ongoingCalls} - new ECC state is:\n" +
+                "-----\n" +
+                $"Ongoing calls: {ongoingCalls} (was: {currentEccState.OngoingCalls})\n" +
+                $"IsMuted: {currentEccState.IsMuted}\n" +
+                $"IsOnHold: {currentEccState.IsOnHold}\n" +
+                $"IsRinging: {currentEccState.IsRinging}\n" +
+                "-----");
+            currentEccState.OngoingCalls = (uint)ongoingCalls;
+
+        });
+        // MuteState is the microphone mute state of the device. 
+        easyCallControl.MuteState.Subscribe((isMuted) =>
+        {
+            OutPutToConsole($"Mute state: {isMuted} - new ECC state is:\n" +
+                "-----\n" +
+                $"Ongoing calls: {currentEccState.OngoingCalls}\n" +
+                $"IsMuted: {isMuted} (was: {currentEccState.IsMuted})\n" +
+                $"IsOnHold: {currentEccState.IsOnHold}\n" +
+                $"IsRinging: {currentEccState.IsRinging}\n" +
+                "-----");
+            currentEccState.IsMuted = isMuted;
+        });
+        // HoldState indicates if call is on hold or not. 
+        easyCallControl.HoldState.Subscribe((isOnHold) =>
+        {
+            OutPutToConsole($"Hold state: {isOnHold} - new ECC state is:\n" +
+                "-----\n" +
+                $"Ongoing calls: {currentEccState.OngoingCalls}\n" +
+                $"IsMuted: {currentEccState.IsMuted}\n" +
+                $"IsOnHold: {isOnHold} (was: {currentEccState.IsOnHold})\n" +
+                $"IsRinging: {currentEccState.IsRinging}\n" +
+                "-----");
+            currentEccState.IsOnHold = isOnHold;
+        });
+        // RingState indicates if the device is ringing or not (incoming call).
+        easyCallControl.RingState.Subscribe((isRinging) =>
+        {
+            OutPutToConsole($"Ring state: {isRinging} - new ECC state is:\n" +
+                "-----\n" +
+                $"Ongoing calls: {currentEccState.OngoingCalls}\n" +
+                $"IsMuted: {currentEccState.IsMuted}\n" +
+                $"IsOnHold: {currentEccState.IsOnHold}\n" +
+                $"IsRinging: {isRinging} (was: {currentEccState.IsRinging})\n" +
+                "-----");
+            currentEccState.IsRinging = isRinging;
+        });
+
+    }
+
+    static void PrintMenu()
+    {
+        Console.WriteLine("Jabra .NET SDK Easy Call Control Sample app starting. Press ctrl+c or close the window to end.");
+        Console.WriteLine("----------");
+        Console.WriteLine("Available commands:");
+        Console.WriteLine("1: New incoming call");
+        Console.WriteLine("2: New outgoing call");
+        Console.WriteLine("3: Answer call");
+        Console.WriteLine("4: End call");
+        Console.WriteLine("5: Hold call");
+        Console.WriteLine("6: Resume call");
+        Console.WriteLine("7: Mute call");
+        Console.WriteLine("8: Unmute call");
+        Console.WriteLine("9: TEMP breakpoint");
+        Console.WriteLine("----------");
+    }
+    static void HandleKeyPress(char keyChar)
+    {
+        switch (keyChar)
+        {
+            case '1':
+                // New incoming call
+                easyCallControl.SignalIncomingCall();
+                OutPutToConsole("SignalIncomingCall()");
+                break;
+            case '2':
+                // New outgoing call
+                easyCallControl.StartCall();
+                OutPutToConsole("StartCall()");
+                break;
+            case '3':
+                // Answer call
+                easyCallControl.AcceptIncomingCall();
+                OutPutToConsole("AcceptIncomingCall()");
+                break;
+            case '4':
+                // End call
+                easyCallControl.EndCall();
+                OutPutToConsole("EndCall()");
+                break;
+            case '5':
+                // Hold call
+                easyCallControl.Hold();
+                OutPutToConsole("Hold()");
+                break;
+            case '6':
+                // Resume call
+                easyCallControl.Resume();
+                OutPutToConsole("Resume()");
+                break;
+            case '7':
+                // Mute call
+                easyCallControl.Mute();
+                OutPutToConsole("Mute()");
+                break;
+            case '8':
+                // Unmute call
+                easyCallControl.Unmute();
+                OutPutToConsole("Unmute()");
+                break;
+            case '9':
+                // Teardown
+                
+                OutPutToConsole("TEMP breakpoint");
+                break;
+            default:
+                // Ignore other keys
+                break;
+        }
+    }
+
+    static void ListenForKeyPress()
+    {
+        while (true)
+        {
+            if (Console.KeyAvailable) // Check if a key press is available
+            {
+                var keyInfo = Console.ReadKey(intercept: true);
+                HandleKeyPress(keyInfo.KeyChar);
+            }
+            else
+            {
+                // Sleep briefly to avoid busy waiting
+                Thread.Sleep(50);
+            }
+        }
+    }
+
+    static void OutPutToConsole(string message)
+    {
+        // Create a new DateTime value for today's date and time
+        DateTime now = DateTime.Now;
+        // create string with current time
+        string time = now.ToString("HH:mm:ss.fff");
+        Console.WriteLine(time + ": " + message);
     }
 }
